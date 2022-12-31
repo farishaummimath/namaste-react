13daff311d152ce3e6dc2ecb2446378e315f67f1
Ummimath Farisha <ummimath.farisha@flipkart.com>
2022-12-31 12:53:59 +0530

    updated folder structure

---
+++ 31-12-2022/session/dist/index.7c0ccee6.js
@@ -0,0 +1,32 @@
+const h1 = React.createElement("h1", {
+    id: "title",
+    key: 1,
+    style: {
+        fontSize: "24px",
+        color: "#875434"
+    }
+}, "Namaste Everyone");
+const h2 = React.createElement("h2", {
+    id: "title2",
+    key: 2,
+    style: {
+        fontSize: "24px",
+        color: "#809344"
+    }
+}, "Welcome to chapter 1");
+const container = React.createElement("div", {
+    id: "container",
+    style: {
+        fontSize: "24px",
+        color: "#809344"
+    }
+}, [
+    h1,
+    h2
+] // passing the container children
+);
+// console.log(h1);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
