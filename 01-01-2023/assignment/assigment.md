65f91fdf40735ac0e845fee3d1cceffcc1b8bf85
Ummimath Farisha <ummimath.farisha@flipkart.com>
2023-01-21 17:35:35 +0530

    day 3 layout foundation

---
+++ 01-01-2023/assignment/assigment.md
@@ -0,0 +1,18 @@
+# what is JSX?
+
+    JSX is a Markup language used in order to create React elements. The markup can be used to represent a standalone single component or a complex component. The syntax for writing JSX is:
+<OpeningTag>...Content...</ClosingTag>
+<OpeningTag />
+A typical component in React would look like:
+<div>This is a div</div> // displays a block of text
+<Clock timezone="gmt" /> // displays a clock with the current time
+
+# Superpowers of JSX
+
+
+ 
+   
+
+
+
+
---
+++ 01-01-2023/session/app.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM, {createRoot} from "react-dom/client";
+
+/**
+ * parcel does Hot module rendering
+ * via file watcher algorithm -C++
+ *
+ *Parcel
+ - Minify
+ -Cleaning code
+ - dev and production build
+ - super fast build algo
+ - image optimization
+ - caching while development
+ - compression
+ - compatible with older versions of browser
+ - HTTPS on dev
+ -- port number
+ -- consistent hashing algo to cache things
+ -- zero config
+ -- packages take care of transitive dependenciess
+ 
+
+ */
+// const h1 = React.createElement(
+// 	"h1",
+// 	{id: "title", key: 1, style: {fontSize: "24px", color: "#875434"}},
+// 	"Namaste Everyone"
+// );
+// const h2 = React.createElement(
+// 	"h2",
+// 	{id: "title2", key: 2, style: {fontSize: "24px", color: "#809344"}},
+// 	"Welcome to chapter 1"
+// );
+// const container = React.createElement(
+// 	"div",
+// 	{
+// 		id: "container",
+
+// 		style: {fontSize: "24px", color: "#809344"},
+// 	},
+// 	[h1, h2] // passing the container children
+// );
+const Title = () => (
+	<h1 id="title" key="h2" tabIndex={1}>
+		Namaste React
+	</h1>
+);
+
+// React Component
+// - Functional component - latest way
+//  Class Based - old way
+
+// console.log(h1);
+const heading = (
+	// <h1 id="title" key="headding-1">
+	<>
+		<h1> Namaste JS from JSX</h1>
+		<h1> Namaste JS from JSX</h1>
+		<h1> Namaste JS from JSX</h1>
+	</>
+);
+// Composing Comopnentss
+
+// Functonal component
+const HeaderComponent = () => {
+	return (
+		<div>
+			<Title />
+			<h2>Namaste React functional component</h2>
+			<h2>This is a h2 tage</h2>
+			{heading}
+		</div>
+	);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(container);
+//passing a react element inside the root
+root.render(<HeaderComponent />);
